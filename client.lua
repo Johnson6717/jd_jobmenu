62aae62c7c96b4cf3feafe591cd8832159e33b9b
Johnson6717 <lacomunidadteespera@gmail.com>
2023-12-04 02:06:03 -0300

    nuevo repositorio

---
+++ client.lua
@@ -0,0 +1,60 @@
+-- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX 
+-- Config --
+Config = {}
+
+Config.Locale = "es"
+Config.OpenKey = 'F3'
+Config.CommandName = 'jobinfo'
+Config.Menu = true
+Config.Command = true
+-- end config --
+
+local ESX = exports['es_extended']:getSharedObject()
+
+if (Config.Menu) == true then
+    RegisterCommand('+openjobmenu', function()
+        openJobMenu()
+    end)
+    RegisterKeyMapping('+openjobmenu', 'Abrir Menú de trabajo', 'keyboard', Config.OpenKey)
+end
+
+if (Config.Command) == true then
+     RegisterCommand(Config.CommandName, function()
+         openJobMenu()
+     end)
+end
+
+
+function openJobMenu()
+        ESX.UI.Menu.CloseAll()
+
+        Job = ((_U('job_name'))  .. ESX.GetPlayerData().job.label)
+        Grade = ((_U('job_grade')) .. ESX.GetPlayerData().job.grade_label)
+        Salary = ((_U('job_salary')) .. ESX.GetPlayerData().job.grade_salary)
+
+    local elements = {
+        {label = Job, value = '0'},
+        {label = Grade, value = '0'},
+        {label = Salary, value = '0'},
+        {label = (_U('close_menu')), value = 'close'}
+    }
+
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'jobmenu',{
+        title = (_U('job_title')),
+        align = 'bottom-right',
+        elements = elements
+    },
+
+function(data, menu)
+        if data.current.value == '0' then
+    elseif data.current.value == 'close' then
+        menu.close()
+    end
+end,
+
+function(data, menu)
+    menu.close()
+    end)
+end
+
+-- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX 
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "adamant"
+game "gta5"
+author "LZ#0001 and Nico 1997#4637"
+
+client_scripts {
+    "client.lua",
+    "config.lua",
+    "@es_extended/locale.lua",
+    "locales/en.lua",
+    "locales/es.lua",
+}
+-- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX 
